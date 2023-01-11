71977081ae777f8aa710d4401072d4a26e3c6bf8
dotdash-ben <ben.samyn@dotdash.ai>
2023-01-11 13:03:57 +0100

    echos

--- scripts/neo4j_docker_init.sh
+++ scripts/neo4j_docker_init.sh
@@ -1,6 +1,7 @@
 # Install Docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
+echo "Docker installed"
 
 sudo docker run \
     --name testneo4jext \
@@ -9,3 +10,4 @@ sudo docker run \
     -e NEO4J_AUTH=none \
     -e 'NEO4JLABS_PLUGINS=["apoc", "graph-data-science"]' \
     neo4j:5.3.0-community
+echo "neo4j container running, give it about 20 seconds to set-up"
