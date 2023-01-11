da692bd0683ae955c84ca820f957d52477c05230
dotdash-ben <ben.samyn@dotdash.ai>
2023-01-11 01:07:12 +0100

    tested lines of code separately

--- scripts/neo4j_docker_init.sh
+++ scripts/neo4j_docker_init.sh
@@ -1,2 +1,10 @@
 # Install Docker
 bash <(curl -fsSL https://get.docker.com)
+
+sudo docker run \
+    --name testneo4jext \
+    -p7474:7474 -p7687:7687 \
+    -d \
+    -e NEO4J_AUTH=none \
+    -e 'NEO4JLABS_PLUGINS=["apoc", "graph-data-science"]' \
+    neo4j:5.3.0-community
